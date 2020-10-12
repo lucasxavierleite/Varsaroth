ce2025f717808b4234afbb816705fdbdcf69efb8
Caio Basso <caioadbasso@hotmail.com>
2020-10-12 14:53:22 -0300

    Added camera and player movement between rooms.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D _playerRB;
+    Vector3 _moveDirection;
+    private float _moveSpeed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _moveDirection = new Vector3(Input.GetAxis("Horizontal") * _moveSpeed, Input.GetAxis("Vertical") * _moveSpeed, 0);
+    }
+
+
+    private void FixedUpdate()
+    {
+        //if (Player.state != dodging)
+        //{
+            MovePlayer(_moveDirection);
+        //}  
+    } 
+
+    void MovePlayer(Vector3 dir)
+    {
+        _playerRB.MovePosition(transform.position + (dir * Time.deltaTime));
+    }
+
+
+    /// <summary>
+    /// This Method moves the player to another room when it comes in contact with a door
+    /// </summary>
+    /// <param name="other"> The trigger that the player collided with</param>
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Door"))
+        {
+            if (other.name == "Door Up")
+            {
+                _playerRB.transform.position += Vector3.up * 90;
+                Camera.main.transform.position += Vector3.up * 216;
+                
+            }
+            else if (other.name == "Door Down")
+            {
+                _playerRB.transform.position += Vector3.down * 90;
+                Camera.main.transform.position += Vector3.down * 216;
+            }
+            else if (other.name == "Door Right")
+            {
+                _playerRB.transform.position += Vector3.right * 100;
+                Camera.main.transform.position += Vector3.right * 384;
+            }
+            else if (other.name == "Door Left")
+            {
+                _playerRB.transform.position += Vector3.left * 100;
+                Camera.main.transform.position += Vector3.left * 384;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RoomStatus.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomStatus : MonoBehaviour
+{
+    public bool _roomVisited;
+    int _enemiesRemaining = -1;
+    // _roomVisited indicates if the room has been visited before
+
+    private void Update()
+    {
+        if (_enemiesRemaining == 0)
+        {
+            for (int a = 0; a < transform.childCount; a++)
+            {
+                if (transform.GetChild(a).tag == "Door")
+                {
+                    Debug.Log(transform.GetChild(a).name);
+                    transform.GetChild(a).gameObject.SetActive(true);
+                }
+            }
+            _enemiesRemaining--;
+        }        
+    }
+
+    private void OnBecameVisible()
+    {
+        if (_roomVisited == false)
+        {
+            _roomVisited = true;
+
+            for (int a = 0; a < transform.childCount; a++)
+            {
+                if (transform.GetChild(a).tag == "Door")
+                {
+                    Debug.Log(transform.GetChild(a));
+                    transform.GetChild(a).gameObject.SetActive(false);
+                }
+            }
+            // _enemiesRemaining = spawnmobs
+        }
+        _enemiesRemaining = 0;
+    }
+
+}
