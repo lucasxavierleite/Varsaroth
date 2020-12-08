4e45e868503ed91db352a15fd1f2e4119437388d
Lucas Xavier Leite <lucasxavierleite@pm.me>
2020-12-08 03:18:22 -0300

    Adding definitive main menu     TO DO: change background image

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenuController : MonoBehaviour
 {
+	[SerializeField]
+    private Color32 menuItemDefaultColor;
+    
+    [SerializeField]
+    private Color32 menuItemHighlightColor;
 
     public void PlayButton()
     {
@@ -17,6 +24,31 @@ public class MainMenuController : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Game is closing");
+    }
+
+    public void ToggleMute(AudioSource music)
+    {
+        music.mute = !music.mute;
+    }
+
+	public void MenuButtonPointerEnter(TextMeshProUGUI text)
+    {
+        text.color = menuItemHighlightColor;
+    }
+    
+    public void MenuButtonPointerExit(TextMeshProUGUI text)
+    {
+        text.color = menuItemDefaultColor;
+    }
+
+	public void MenuButtonPointerEnter(Image image)
+    {
+        image.color = menuItemHighlightColor;
+    }
+    
+    public void MenuButtonPointerExit(Image image)
+    {
+        image.color = menuItemDefaultColor;
     }
 
 
